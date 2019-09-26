6fce3ddc1443d7dad22e416095c9439e7f5e4d72
glesssard <lesg2605@usherbrooke.ca>
2019-09-26 11:43:37 -0400

    First try of max constant velocity per radius

--- Math scripts/ConstantSpeedTurn.py
+++ Math scripts/ConstantSpeedTurn.py
@@ -1,19 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as mplt
 
 
-def max_velocity_constant_turn(angular_velocity, turn_radius):
-    R = 68.17
-    delta_x = 20
+def max_velocity_constant_turn(turn_radius):
+    R = 68.17/1000
+    delta_x = 20/1000
     g = 9.81
+    w_z = np.sqrt(g/turn_radius)
 
     theta = np.arcsin(delta_x/R)
     delta_z = R - R*np.cos(theta)
 
     phi = np.arctan(delta_x/delta_z)
 
-    angular_velocity_z = angular_velocity/np.cos(np.pi/2 - phi)
+    angular_velocity = w_z / np.sin(np.pi/2 - phi)
 
-    return np.pi
+    return turn_radius * angular_velocity
 
 
-max_velocity_constant_turn(10,30)
+# Testing
+number_of_points = 1000
+turn_radi = list(np.linspace(0, 2, number_of_points))
+speeds = np.empty(number_of_points)
+
+for turn_radius in turn_radi:
+    if turn_radius != 0:
+        i = turn_radi.index(turn_radius)
+        speeds[i] = max_velocity_constant_turn(turn_radius=turn_radius)
+
+
+mplt.stem(turn_radi, speeds)
+mplt.show()
