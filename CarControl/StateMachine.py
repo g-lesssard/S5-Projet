57c13b30144ee4ee1ef998a9855a30619e6fe839
glesssard <lesg2605@usherbrooke.ca>
2019-11-27 14:11:56 -0500

    Modified angles for line following and added pietons savng behaviours

--- CarControl/StateMachine.py
+++ CarControl/StateMachine.py
@@ -11,6 +11,7 @@ import time
 class State(Enum):
     BASE_LINE_FOLLOWER = auto()
     AVOID_OBSTACLE = auto()
+    PIETONS = auto()
     LOOP = auto()
     DO_STOP = auto()
     TEST = auto()
@@ -43,20 +44,26 @@ class Event(object):
 class StateController(object):
 
     def __init__(self, printing=False):
-        self.state = State.TEST
+        self.state = State.BASE_LINE_FOLLOWER
         self.radar = Sensors.Radar(printing=False)
         self.line_follower = Sensors.Line_Follower(printing=False)
         self.dir_control = DirectionControl(printing=printing)
+        self.angle = 0
 
     def startReadingThreads(self):
         self.radar.startReading()
         self.radar.addObserver(self.objectDetected)
         self.line_follower.startReading()
+        self.line_follower.addObserver(self.pietonsDetected)
         
 
     def objectDetected(self):
         print("Obstacle detected, setting avoidance course...")
         self.state = State.AVOID_OBSTACLE
+    
+    def pietonsDetected(self):
+        print("Pietons detected, stopping...")
+        self.state = State.PIETONS
 
         
 
@@ -72,7 +79,10 @@ class StateController(object):
         if self.state is State.BASE_LINE_FOLLOWER:
             self.follow_line()
         if self.state is State.AVOID_OBSTACLE:
-            self.avoidObstacle()            
+            self.avoidObstacle()
+        if self.state is State.PIETONS:
+            self.savePietons()
+
 
     def stop(self):
         self.radar.stopReading()
@@ -81,30 +91,29 @@ class StateController(object):
 
     def follow_line(self):
         self.dir_control.setSpeed(60)
-        angle = 0
         refs = self.line_follower.getData()
         print(refs)
         if refs == [0,0,1,0,0]:
-            angle = -0
+            self.angle = -0
         elif refs == [0,0,0,0,1]:
-            angle = 45
+            self.angle = 45
         elif refs == [0,0,0,1,1]:
-            angle = 35
+            self.angle = 35
         elif refs == [0,0,0,1,0]:
-            angle = 25
+            self.angle = 20
         elif refs == [0,0,1,1,0]:
-            angle = 15
+            self.angle = 10
         elif refs == [1,0,0,0,0]:
-            angle = -45
+            self.angle = -45
         elif refs == [1,1,0,0,0]:
-            angle = -35
+            self.angle = -35
         elif refs == [0,1,0,0,0]:
-            angle = -25
+            self.angle = -20
         elif refs == [0,1,1,0,0]:
-            angle = -15
+            self.angle = -10
         else:
-            angle = 45
-        self.dir_control.turn(angle)
+            pass
+        self.dir_control.turn(self.angle)
 
     def avoidObstacle(self):
         time.sleep(0.1)
@@ -112,6 +121,14 @@ class StateController(object):
         time.sleep(2)
         print("Obstacle avoided, going back to line following")
         self.state = State.BASE_LINE_FOLLOWER
+
+    def savePietons(self):
+        self.dir_control.setSpeed(30)
+        time.sleep(2)
+        self.dir_control.setSpeed(0)
+        time.sleep(1)
+        self.state = State.BASE_LINE_FOLLOWER
+
 
 
         
